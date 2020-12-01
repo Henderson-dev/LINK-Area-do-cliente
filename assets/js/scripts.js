a696e9db6aad876732473fefbad034afc09da0a8
Henderson-dev <teconet.arte@gmail.com>
2020-12-01 19:08:33 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,43 @@
+(function ($) {
+
+
+// fixar menu na tela
+// var offset = $('.side-menu').offset().top;
+// var $meuMenu = $('.side-menu'); // guardar o elemento na memoria para melhorar performance
+// $(document).on('scroll', function () {
+//     if (offset <= $(window).scrollTop()) {
+//         $meuMenu.addClass('fixar');
+//     } else {
+//         $meuMenu.removeClass('fixar');
+//     }
+// });
+
+
+   /**
+   * 
+   * Menu Hamburguer 
+   * 
+   * forEach implementation for Objects/NodeLists/Arrays, automatic type loops and context options
+   *
+   * @private
+   * @author Todd Motto
+   * @link https://github.com/toddmotto/foreach
+   * @param {Array|Object|NodeList} collection - Collection of items to iterate, could be an Array, Object or NodeList
+   * @callback requestCallback      callback   - Callback function for each iteration.
+   * @param {Array|Object|NodeList} scope=null - Object/NodeList/Array that forEach is iterating over, to use as the this value when executing callback.
+   * @returns {}
+   */
+  var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+  var hamburgers = document.querySelectorAll(".hamburger");
+  if (hamburgers.length > 0) {
+    forEach(hamburgers, function(hamburger) {
+      hamburger.addEventListener("click", function() {
+        this.classList.toggle("is-active");
+      }, false);
+    });
+  }
+
+
+})(jQuery);
+
